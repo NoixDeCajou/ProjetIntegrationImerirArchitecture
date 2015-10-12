32306cac9ae6dce67eaf8e474888eb1cd29f114d
PancakeInvaders <mickael.campoy@imerir.com>
2015-10-12 16:29:36 +0200

    server joignable depuis le reseau

--- flaskSocketIO_tuto/untitled1.py
+++ flaskSocketIO_tuto/untitled1.py
@@ -7,26 +7,39 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/test/')
+def index2():
+    return render_template('index2.html')
+
+
 @socketio.on('my event', namespace='/test')
 def test_message(message):
+    print("my event: " + message['data'])
     emit('my response', {'data': message['data']})
+
 
 @socketio.on('my broadcast event', namespace='/test')
 def test_message(message):
+    print("my broadcast: " + message['data'])
     emit('my response', {'data': message['data']}, broadcast=True)
+
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
     print("on connect")
     emit('my response', {'data': 'Connected'})
 
+
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     print('Client disconnected')
 
+
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, '0.0.0.0')
