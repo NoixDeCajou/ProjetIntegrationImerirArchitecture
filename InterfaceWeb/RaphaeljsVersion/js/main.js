dd39c98d05cfe671b64b13113508bfae4c4e1d74
PancakeInvaders <mickael.campoy@imerir.com>
2015-10-14 14:40:12 +0200

    recherche point le plus proche

--- InterfaceWeb/RaphaeljsVersion/js/main.js
+++ InterfaceWeb/RaphaeljsVersion/js/main.js
@@ -5,120 +5,132 @@ var firstArea = jsonCity.areas[0];
 var areaName = firstArea.name;
 $("#areaName").append(areaName);
 
+var paperGlobal;
+
+
 var listVerticesCircle = [];
 
 
 window.onresize=function(){    
-    var canvas = init();
-    canvas.innerHTML = "";
-    draw(canvas);
+	var canvas = init();
+	canvas.innerHTML = "";
+	draw(canvas);
 }
 
 window.onload = function(){
-    var canvas = init();
-    draw(canvas);
+	var canvas = init();
+	draw(canvas);
 };
 
 function init(){
-    canvas = document.getElementById("myCanvas");
-    canvas.width = document.body.clientWidth; //document.width is obsolete
-    canvas.height = document.body.clientHeight; //document.height is obsolete
-    canvasW = canvas.width;
-    canvasH = canvas.height;
-    return canvas;
+	canvas = document.getElementById("myCanvas");
+	canvas.width = document.body.clientWidth; //document.width is obsolete
+	canvas.height = document.body.clientHeight; //document.height is obsolete
+	return canvas;
 }
 
 
 function draw(canvas){
-// Creates canvas 320 × 200 at 10, 50
-var paper = Raphael(canvas, canvas.width, canvas.height);
+	paperGlobal = Raphael(canvas, canvas.width, canvas.height);
 
-var w = canvas.width;
-var h = canvas.height;
-
+	var widthCanvas = canvas.width;
+	var heightCanvas = canvas.height;
 
 
-// var c = paper.path("M 0,0 L "+canvas.width*0.5+" "+canvas.height*0.25);
-// //var c = paper.path("M10 10L90 90");
-// // Sets the stroke attribute of the circle to white
-// c.attr({stroke:'#000000',"stroke-width":5});
-// // c.glow({color: '#f00',width: 5});
+	$.each(firstArea.map.vertices, function(i,item){
+		var circle = paperGlobal.circle(widthCanvas*item.x, heightCanvas*item.y, 30);
+		listVerticesCircle[item.name]=item;
+		//var text = paper.text(canvas.width*item.x, canvas.height*item.y, item.name);
+		//text.attr({"font-weight": "bold" , "font-size" : 20});
+		circle.attr({stroke:'#888888',"stroke-width":5,"fill": "#f00",cursor: 'pointer'});
+		circle.click(function(e) {
+			e.stopPropagation();
+			appelSurPoint(this,item.name);
+		});
+		circle.hover(hoverIn, hoverOut);
+	});
 
-// // Creates circle at x = 50, y = 40, with radius 10
-// var circle = paper.circle(canvas.width*0.5, canvas.height*0.25, 10);
-// // Sets the fill attribute of the circle to red (#f00)
-// circle.attr("fill", "#f00");
 
-  $.each(firstArea.map.vertices, function(i,item){
-    // console.log(i +" item : "+item);
-    // console.log(item.name +"  x: "+ item.x +"   y :"+ item.y);
-    // Creates circle at x = 50, y = 40, with radius 10
-    var circle = paper.circle(canvas.width*item.x, canvas.height*item.y, 30);
-    listVerticesCircle[item.name]=item;
-    //var text = paper.text(canvas.width*item.x, canvas.height*item.y, item.name);
-    //text.attr({"font-weight": "bold" , "font-size" : 20});
-    // Sets the fill attribute of the circle to red (#f00)
-    circle.attr({"fill": "#f00",cursor: 'pointer'});
-    circle.hover(hoverIn, hoverOut);
-    
-  });
+	$.each(firstArea.map.streets, function(i,item){
+		var pointA = listVerticesCircle[item.path[0]];
+		var pointB = listVerticesCircle[item.path[1]];
+		console.log(pointA);
+		var c = paperGlobal.path("M "+widthCanvas*pointA.x+","+heightCanvas*pointA.y+" L"+widthCanvas*pointB.x+","+heightCanvas*pointB.y);
+		// Sets the stroke attribute of the circle to white
+		c.attr({stroke:'#888888',"stroke-width":30});
+		c.toBack();
+	});
 
-/*****ANIMATIONS*****/
-// Hover in function
-function hoverIn() {
-  this.animate({
-    r: 40
-  }, 500,"bounce");
+	console.log(listVerticesCircle);
+
+		/*****ANIMATIONS*****/
+	// Hover in function
+	function hoverIn() {
+		this.animate({
+			r: 40
+		}, 500,"bounce");
+	}
+
+	// Hover out function
+	function hoverOut() {
+		this.animate({
+			r: 30
+		}, 500,"bounce");
+	}
+
+	// Hover in function
+	function hoverInPath() {
+		this.animate({
+			stroke : "#f00"
+		}, 500,"bounce");
+	}
+
+	// Hover out function
+	function hoverOutPath() {
+		this.animate({
+			stroke : "#000000"
+		}, 500,"bounce");
+	}
+
 }
 
-// Hover out function
-function hoverOut() {
-  this.animate({
-    r: 30
-  }, 500,"bounce");
-}
-
-// Hover in function
-function hoverInPath() {
-  this.animate({
-    stroke : "#f00"
-  }, 500,"bounce");
-}
-
-// Hover out function
-function hoverOutPath() {
-  this.animate({
-    stroke : "#000000"
-  }, 500,"bounce");
-}
-
-
-// circle.hover(hoverIn, hoverOut);
-// c.hover(hoverInPath, hoverOutPath);
-
-
-
-
-
-
-
-
-
-$.each(firstArea.map.streets, function(i,item){
-  var pointA = listVerticesCircle[item.path[0]];
-  var pointB = listVerticesCircle[item.path[1]];
-  console.log(pointA);
-  var c = paper.path("M "+w*pointA.x+","+h*pointA.y+" L"+w*pointB.x+","+h*pointB.y);
-  // Sets the stroke attribute of the circle to white
-  c.attr({stroke:'#777777',"stroke-width":30});
-  c.toBack();
+$('#myCanvas').click(function(e) {
+	var posX = $(this).position().left,posY = $(this).position().top;
+	posX = (e.pageX - posX);
+	posY = (e.pageY - posY);
+	alert(posX + " , "+posY);
+	getPointProche(posX,posY,this);
 });
 
-console.log(listVerticesCircle);
+function appelSurPoint(caller,name){
+	console.log(caller);
+	alert("Un Taxi a été appelé sur le point "+name);
+}
+
+function calculeDistance(x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow((y2 - y1),2) + Math.pow((x2 - x1),2));
+}
+
+function getPointProche(posX,posY){
+	var distanceLaPlusProche = 9999;
+	var pointLePlusProche;
+	canvas = document.getElementById("myCanvas");
+	var widthCanvas = canvas.width;
+	var heightCanvas = canvas.height;
+	$.each(firstArea.map.vertices,function(i,item){
+		var distance = calculeDistance(posX,posY,widthCanvas*item.x,heightCanvas*item.y);
+		if (distance < distanceLaPlusProche) {
+			distanceLaPlusProche = distance;
+			pointLePlusProche = item;
+		};
+	});
+	console.log(pointLePlusProche);
+	console.log(distanceLaPlusProche);
+
+	var c = paperGlobal.path("M "+posX+","+posY+" L"+widthCanvas*pointLePlusProche.x+","+heightCanvas*pointLePlusProche.y);
+	// Sets the stroke attribute of the circle to white
+	c.attr({"stroke-dasharray" :"-","stroke-width":3});
+	c.toBack();
 
 }
 
-    $('#myCanvas').click(function(e) {
-        var posX = $(this).position().left,posY = $(this).position().top;
-        alert( (e.pageX - posX) + ' , ' + (e.pageY - posY));
-    });
