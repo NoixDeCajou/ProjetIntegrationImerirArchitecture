0443e239cbf6c573c8440a22bbbb3a4fbae2c6bf
PancakeInvaders <mickael.campoy@imerir.com>
2015-10-19 10:48:27 +0200

    Ajout du support des boutons

--- Arduino/ArduinoIntegration/ArduinoIntegration.ino
+++ Arduino/ArduinoIntegration/ArduinoIntegration.ino
@@ -3,8 +3,22 @@
 #include <SPI.h>
 #include <WebSocketClient.h>
 #include <aJSON.h>
+#include <LiquidCrystal.h>
+
+#define btnRIGHT  0
+#define btnUP     1
+#define btnDOWN   2
+#define btnLEFT   3
+#define btnSELECT 4
+#define btnNONE   5
 
 byte mac[] = {  0x98, 0x4F, 0xEE, 0x05, 0x34, 0x14 };
+
+//LCD
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+int lcd_key     = 0;
+int adc_key_in  = 0;
+
 
 IPAddress server(192,168,1,69); // serveur
 char WSserver[] = "192.168.1.69";
@@ -28,11 +42,15 @@ boolean startRead = false; // is reading?
 /*****Definition Fonction******/
 int parseJson(char *jsonString) ;
 void initWebSocket(int);
+int read_LCD_buttons();
 
 /******************************/
 
 
 void setup() {
+   lcd.begin(16, 2);              // start the library
+   lcd.setCursor(0,0);
+   lcd.print("> Initialisation ..."); // print a simple message
   system("ifconfig > /dev/ttyGS0"); //affiche la configuration reseau
   
   Serial.begin(9600);
@@ -51,6 +69,7 @@ void setup() {
    // if you get a connection, report back via serial:
   if (client.connect(server, 5000)) {
     Serial.println("connected");
+    
     // Make a HTTP request:
     client.println("GET /cab HTTP/1.1");
     client.println();
@@ -59,10 +78,8 @@ void setup() {
     // if you didn't get a connection to the server:
     Serial.println("connection failed");
   }
-
-}
-
-void loop() {
+  
+  
   String jsonHttpGet = readPage();
   Serial.println(jsonHttpGet);
   
@@ -75,47 +92,69 @@ void loop() {
     if (value) {
         Serial.println("Successfully Parsed: ");
         Serial.println(value);
-        initWebSocket(value);
+        
+        initWebSocket();
         //WSclient.monitor();
         
     } else {
         Serial.println("There was some problem in parsing the JSON");
     }
-    
-    WSclient.monitor();
-  
-    //delay(2000);
-  //si le client est connecté
-  /*if(client.connected() && httpRequestDone == false){
-    //Serial.println("Loop : connected == true");
-    //Serial.println("ClientConnected");
-    //si il y a des données a lire ...
-    if (client.available()) {
-      //Serial.println("Loop : Données disponibles !");
-      char c = client.read();
-      requete += c;
-      //Serial.print(c);
-    }
-        Serial.print(requete);
-        //httpRequestDone = true;
 
-  }
-  
-  //si deconnexion
-  else {
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
+}
 
-    // do nothing forevermore:
-    for(;;)
-      ;
-  }*/
-  
+void loop() {
+ //WSclient.monitor();
+ lcd.setCursor(9,1);            // move cursor to second line "1" and 9 spaces over
+ lcd.print(millis()/1000);      // display seconds elapsed since power-up
+
+
+ lcd.setCursor(0,1);            // move to the begining of the second line
+ lcd_key = read_LCD_buttons();  // read the buttons
+
+ switch (lcd_key)               // depending on which button was pushed, we perform an action
+ {
+   case btnRIGHT:
+     {
+     lcd.print("RIGHT ");
+     Serial.print("RIGHT ");
+     break;
+     }
+   case btnLEFT:
+     {
+     lcd.print("LEFT   ");
+     Serial.print("LEFT ");
+     break;
+     }
+   case btnUP:
+     {
+     lcd.print("UP    ");
+     Serial.print("UP ");
+     break;
+     }
+   case btnDOWN:
+     {
+     lcd.print("DOWN  ");
+     Serial.print("DOWN ");
+     break;
+     }
+   case btnSELECT:
+     {
+     lcd.print("SELECT");
+     Serial.print("SELECT ");
+     break;
+     }
+     case btnNONE:
+     {
+     lcd.print("NONE  ");
+     Serial.print("NONE ");
+     break;
+     }
+ }
+ WSclient.monitor();
 }
 
 String readPage(){
-  //read the page, and capture & return everything between '<' and '>'
+  //read the page, and capture & return everything between '{' and '}'
 
   stringPos = 0;
   memset( &inString, 0, 32 ); //clear inString memory
@@ -125,13 +164,13 @@ String readPage(){
     if (client.available()) {
       char c = client.read();
 
-      if (c == '{' ) { //'<' is our begining character
+      if (c == '{' ) { //'{' is our begining character
         startRead = true; //Ready to start reading the part 
           inString[stringPos] = '{';
           stringPos ++;
       }else if(startRead){
 
-        if(c != '}'){ //'>' is our ending character
+        if(c != '}'){ //'}' is our ending character
           inString[stringPos] = c;
           stringPos ++;
         }else{
@@ -184,13 +223,41 @@ int parseJson(char *jsonString)
 }
 
 
+// read the buttons
+int read_LCD_buttons()
+{
+ adc_key_in = analogRead(0);      // read the value from the sensor 
+ // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+ // we add approx 50 to those values and check to see if we are close
+ if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+ // For V1.1 us this threshold
+ if (adc_key_in < 50)   return btnRIGHT;  
+ if (adc_key_in < 250)  return btnUP; 
+ if (adc_key_in < 450)  return btnDOWN; 
+ if (adc_key_in < 650)  return btnLEFT; 
+ if (adc_key_in < 850)  return btnSELECT;  
 
-void initWebSocket(int port){
-  WSclient.connect(WSserver,port);
+ // For V1.0 comment the other threshold and use the one below:
+/*
+ if (adc_key_in < 50)   return btnRIGHT;  
+ if (adc_key_in < 195)  return btnUP; 
+ if (adc_key_in < 380)  return btnDOWN; 
+ if (adc_key_in < 555)  return btnLEFT; 
+ if (adc_key_in < 790)  return btnSELECT;   
+*/
+
+
+ return btnNONE;  // when all others fail, return this...
+}
+
+
+
+void initWebSocket(){
+  WSclient.connect(WSserver,8000);
   WSclient.onOpen(onOpen);
   WSclient.onMessage(onMessage);
   WSclient.onError(onError);
-  //WSclient.send("Hello World!");
+  Serial.println("WebSocket OK...");
 }
 
 void onOpen(WebSocketClient client) {
@@ -200,7 +267,6 @@ void onOpen(WebSocketClient client) {
 
 void onMessage(WebSocketClient client, char* message) {
   Serial.println("EXAMPLE: onMessage()");
-  client.send("monmessage");
   Serial.print("Received: "); Serial.println(message);
 }
 
