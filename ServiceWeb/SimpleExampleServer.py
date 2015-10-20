012e5a0997fbef8230ed1e05f2bc76adefbcfa87
PancakeInvaders <mickael.campoy@imerir.com>
2015-10-20 17:55:13 +0200

    Serveur: Acceptation des requetes par le taxi, changements dans le mode

--- ServiceWeb/SimpleExampleServer.py
+++ ServiceWeb/SimpleExampleServer.py
@@ -101,6 +101,12 @@ def messageReceived(msg):
 
 
                 for req in rootObject['cabRequest']:
+
+                    print("(req['idCabRequest', idRequest, req['idCabRequest'] == idRequest   :")
+                    print(req['idCabRequest'])
+                    print(idRequest)
+                    print(req['idCabRequest'] == idRequest)
+
                     if req['idCabRequest'] == idRequest:
                         theShortestPath = Dijkstra.doDijkstra(graphMap,
                                                               unicode(str(
@@ -122,7 +128,7 @@ def messageReceived(msg):
             # supprimer la requete avec l'id idRequest de la liste de requetes
             for req in rootObject["cabRequest"]:
 
-                if req["idCabRequest"] == jsonReceived['id']:
+                if req["idCabRequest"] == idRequest:
                     rootObject["cabRequest"].remove(req)
                     pass
                 pass
